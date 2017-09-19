c5e157580ede74830926bef5e07934f5dea87fca
Corey <CoreyOuellette.co@gmail.com>
2017-09-19 09:27:31 -0400

    Update pie.js

--- pie/pie.js
+++ pie/pie.js
@@ -19,21 +19,18 @@ var pie = d3.layout.pie()
     .sort(null)
     .value(function(d) { return d.amount; });
 
-var Piesvg = d3.select("[LOCATION]").append("svg"). // Define where you want to place the graph
+var Piesvg = d3.select("[LOCATION]").append("svg") // Define where you want to place the graph
     .attr("width", width)
     .attr("height", height)
   .append("g")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-d3.csv("[DEFINE THE DATA LOCATION]", function(err, data) {. // Define where the data is stored
+d3.csv("[DEFINE THE DATA LOCATION]", function(err, data) { // Define where the data is stored
 
   var sentimentCount = d3.nest()
-    .key(function(d) { return d.["WHAT SHOULD WE MEASURE?"]; }). // Quantify non-numeric data into numbers
+    .key(function(d) { return d.["WHAT SHOULD WE MEASURE?"]; }) // Quantify non-numeric data into numbers
     .rollup(function(v) { return v.length; })
     .entries(data);
-
-
-
 
 sentimentCount.forEach(function(d) {
     d.["WHAT SHOULD WE MEASURE?"] = d.key;  // Sum the non numeric values togethor by category
@@ -47,7 +44,7 @@ sentimentCount.forEach(function(d) {
       .data(pie(sentimentCount)).enter()
   .append("path")
       .attr("d", arc)
-      .style("fill", function(d) {  return color(d.data.["WHAT SHOULD WE MEASURE?"]); }). //Associate the colour by category
+      .style("fill", function(d) {  return color(d.data.["WHAT SHOULD WE MEASURE?"]); }) //Associate the colour by category
       .each(function(d) { this._current = d; });
 
 
